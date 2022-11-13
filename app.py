56990334f48d28b9934a82b1a11ba4b44224f3a2
jorge juan dominguez <jorgejuandominguez@gmail.com>
2022-11-13 16:49:18 +0100

    mejoras parte grafica v1

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] =  'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] =  'coltis_materias'
+app.config['MYSQL_DB'] =  'universidad'
 app.config['MYSQL_PORT'] =  3308
 mysql = MySQL(app)
 
